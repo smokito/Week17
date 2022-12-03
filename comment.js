0ec275be52764644fdc4be1fcb6f85d52482e530
smokito <anna.aksiuk.aa@gmail.com>
2022-12-03 22:47:46 +0100

    added changes in comment

--- comment.js
+++ comment.js
@@ -3,27 +3,25 @@ btn.addEventListener("click", addComment);
 let input = document.querySelector(".input");
 let comments = [];
 
-function addComment() {
+function checkSpam() {
   let result = input.value;
   let text = result.toLowerCase();
   console.log(text);
 
-  /*if (text.includes("viagra")) {
+  if (text.includes("viagra")) {
     text = text.replace("viagra", "***");
-    alert(comment1);
-    comments.push(comment);
-  } else if (resultLowCase.search("xxx")) {
-    comment2 = resultLowCase.replace("xxx", "***");
-    alert(comment2);
-    comments.push(comment2);
+    comments.push(text);
+  } else if (text.includes("xxx")) {
+    text = text.replace("xxx", "***");
+    comments.push(text);
   } else {
-    alert(resultLowCase);
-    comments.push(resultLowCase);
-  }*/
-  comments.push(text);
+    comments.push(text);
+  }
+
   console.log(comments);
   generateComments();
   input.value = "";
+  comments = [];
 }
 
 function generateComments() {
